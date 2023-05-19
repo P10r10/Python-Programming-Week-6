288c5d55eddd8babbeee5d15dc78178e8c73efcf
P10r10 <asantiago@keemail.me>
2023-05-19 15:03:59 +0100

    recipe_search

--- recipe_search.py
+++ recipe_search.py
@@ -1,8 +1,4 @@
-def get_recipes(file: str) -> list:
-    with open(file) as fp:
-        return [row.strip() for row in fp]
-
-def search_by_name(filename: str, word: str) -> list:
+def get_recipes(filename: str) -> list:
     with open(filename) as fp:
         recipes = [row.strip() for row in fp]
         names = []
@@ -10,10 +6,26 @@ def search_by_name(filename: str, word: str) -> list:
         for i in range(1, len(recipes)):
             if recipes[i] == "":
                 names.append(recipes[i + 1])
+        return names
+
+def search_by_name(filename: str, word: str) -> list:
+    names = get_recipes(filename)
     result = []
     for name in names:
         if word.upper() in name.upper():
             result.append(name)
     return result
 
-print(search_by_name("recipes1.txt", "cake"))
+def search_by_time(filename: str, prep_time: int):
+    with open(filename) as fp:
+        recipes = [row.strip() for row in fp]
+        result = []
+        for i in range(len(recipes)):
+            if recipes[i].isdigit() and int(recipes[i]) <= prep_time:
+                result.append(f"{recipes[i - 1]}, preparation time {recipes[i]} min")
+        return result
+
+found_recipes = search_by_time("recipes1.txt", 61)
+
+for recipe in found_recipes:
+    print(recipe)
