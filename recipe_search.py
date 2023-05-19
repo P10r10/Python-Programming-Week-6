b91772ae63d9f8fe61c715f634b249bcdbecada7
P10R10 <asantiago@keemail.me>
2023-05-20 00:00:24 +0100

    recipe_search

--- recipe_search.py
+++ recipe_search.py
@@ -8,6 +8,7 @@ def get_recipes(filename: str) -> list:
                 names.append(recipes[i + 1])
         return names
 
+
 def search_by_name(filename: str, word: str) -> list:
     names = get_recipes(filename)
     result = []
@@ -16,16 +17,39 @@ def search_by_name(filename: str, word: str) -> list:
             result.append(name)
     return result
 
+
 def search_by_time(filename: str, prep_time: int):
     with open(filename) as fp:
         recipes = [row.strip() for row in fp]
         result = []
         for i in range(len(recipes)):
             if recipes[i].isdigit() and int(recipes[i]) <= prep_time:
-                result.append(f"{recipes[i - 1]}, preparation time {recipes[i]} min")
+                result.append(
+                    f"{recipes[i - 1]}, preparation time {recipes[i]} min")
         return result
 
-found_recipes = search_by_time("recipes1.txt", 61)
 
-for recipe in found_recipes:
-    print(recipe)
+def search_by_ingredient(filename: str, ingredient: str):
+    with open(filename) as fp:
+        lines = [row.strip() for row in fp]
+        lines.append("")
+    recipes = []
+    recipe = []
+    for row in lines:
+        if row != "":
+            recipe.append(row)
+        else:
+            recipes.append(recipe.copy())
+            recipe.clear()
+    result = []
+    for recipe in recipes:
+        if ingredient in recipe:
+            result.append(f"{recipe[0]}, preparation time {recipe[1]} min")
+    return result
+
+
+if __name__ == "__main__":
+    found_recipes = search_by_ingredient("recipes1.txt", "eggs")
+
+    for recipe in found_recipes:
+        print(recipe)
