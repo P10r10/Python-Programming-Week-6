eb834ffb8babd63b8c7c60b51d485061b5bf3385
P10r10 <asantiago@keemail.me>
2023-05-25 12:25:52 +0100

    stuff

---
+++ stuff3.py
@@ -0,0 +1,8 @@
+pizzas = ["pepperoni", "napolitana", "cheeseham"]
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print("I really love pizza!")
+animals = ["cat", "dog", "turtle"]
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+print("Any of these animals would make a great pet.")
---
+++ stuff4.py
@@ -0,0 +1,6 @@
+for n in range(1, 21):
+    print(n, " ", end="")
+million = [n for n in range(1, 1_000_001)]
+print("\nmin: ", min(million))
+print("max: ", max(million))
+print("sum: ", sum(million))
