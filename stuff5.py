40e55e71761ab8a3f457aa3b3f6fc585c31fecab
P10R10 <asantiago@keemail.me>
2023-05-31 23:15:44 +0100

    stuff5

--- stuff5.py
+++ stuff5.py
@@ -1,4 +1,5 @@
 alien_color = "red"
+points = 0
 if alien_color == "green":
     points = 5
 elif alien_color == "yellow":
@@ -6,5 +7,19 @@ elif alien_color == "yellow":
 elif alien_color == "red":
     points = 15
 print(f"You won {points} points!")
-
-# TODO 5.6 p. 84
+age = 80
+if age < 2:
+    print("You're a baby!")
+elif age < 4:
+    print("You're a toddler.")
+elif age < 13:
+    print("You're a kid.")
+elif age < 20:
+    print("You're a teenager.")
+elif age < 65:
+    print("You're an adult.")
+else:
+    print("You're an elser.")
+fruits = ["mango", "banana", "peach"]
+if "banana" in fruits:
+    print("You really like bananas!")
