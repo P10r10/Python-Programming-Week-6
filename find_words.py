f9bad7aa8727e191542ad869d1a7c25347ec0122
P10r10 <asantiago@keemail.me>
2023-06-02 15:39:46 +0100

    find words

--- find_words.py
+++ find_words.py
@@ -6,16 +6,18 @@ def get_words_from_file() -> list:
 def find_words(search_term: str) -> list:
     result = []
     for word in get_words_from_file():
+        split = search_term.split(".")
         if (
             word == search_term
             or (search_term[-1] == "*" and word.startswith(search_term[:-1]))
             or (search_term[0] == "*" and word.endswith(search_term[1:]))
-            or (word.startswith(search_term.split(".")[0])
-                and word.endswith(search_term.split(".")[-1])
+            or (word.startswith(split[0])
+                and word.endswith(split[-1])
+                # and split[1] == word[len(split[0])+1: len(word)-len(split[2])-1]
                 and len(word) == len(search_term))
         ):
             result.append(word)
     return result
 
+
 print(find_words("c.d."))
-# Search term c.d. should return 7 lines
