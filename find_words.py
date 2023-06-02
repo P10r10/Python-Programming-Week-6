dc1ac3865bec86b7d21b1569c14e65e74d56586e
P10r10 <asantiago@keemail.me>
2023-06-02 11:29:17 +0100

    find words

--- find_words.py
+++ find_words.py
@@ -10,25 +10,12 @@ def find_words(search_term: str) -> list:
             word == search_term
             or (search_term[-1] == "*" and word.startswith(search_term[:-1]))
             or (search_term[0] == "*" and word.endswith(search_term[1:]))
+            or (word.startswith(search_term.split(".")[0])
+                and word.endswith(search_term.split(".")[-1])
+                and len(word) == len(search_term))
         ):
             result.append(word)
     return result
 
-
-# print(find_words("*ulet"))
-words = [".."]
-
-for c in range(97, 123):
-    words.append(words[0].replace(".", chr(c), 1))
-# del(words[0])
-words.remove("..")
-for word in words:
-    if "." in word:
-        for c in range(97, 123):
-            words.append(word.replace(".", chr(c), 1))
-        words.remove(word)
-print(words)       
-
-# for letter in word:
-#     if letter == ".":
-# TODO arranjar algoritmo para substituição do "."
+print(find_words("c.d."))
+# Search term c.d. should return 7 lines
