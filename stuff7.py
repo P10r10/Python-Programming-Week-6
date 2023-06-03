6d3160362321924d0f1ef2b5f4f26540f57054da
P10R10 <asantiago@keemail.me>
2023-06-03 09:41:23 +0100

    stuff7

--- stuff7.py
+++ stuff7.py
@@ -10,11 +10,36 @@ for k, v in person1.items():
 
 countries = {
     "portugal": "lisbon",
-    "espanha": "madrid",
-    "frança": "paris",
-    "alemanha": "berlin",
-    "bélgica": "bruxels",
+    "spain": "madrid",
+    "france": "paris",
+    "germany": "berlin",
+    "belgium": "bruxels",
+    "mali": "bamaco",
+    "italy": "rome",
+    "croatia": "zagreb",
+    "morocco": "rabat",
+    "chile": "santiago",
+
 }
 
 for country, capital in countries.items():
     print(f"The capital of {country.title()} is {capital.title()}.")
+for country in countries.keys():
+    print(country.title())
+for capital in countries.values():
+    print(capital.title())
+
+people = ["alice", "peter", "john", "elsa"]
+persons = {
+    "alice": "lisbon",
+    "bob": "madrid",
+    "michael": "paris",
+    "jeremy": "berlin",
+    "elsa": "bruxels",
+}
+for name in people:
+    if name in persons:
+        print(f"Thank you for answering the poll, {name.title()}!")
+    else:
+        print(f"Hi, {name.title()}, please answer the poll.")
+
