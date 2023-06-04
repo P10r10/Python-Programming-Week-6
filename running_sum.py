7865dcba69d15f183c3453206bee8df1a8bd326d
P10R10 <asantiago@keemail.me>
2023-06-04 11:36:51 +0100

    running sum

--- running_sum.py
+++ running_sum.py
@@ -1,10 +1,6 @@
 def runningSum(nums: list[int]) -> list[int]:
-    sum = 0
-    res = []
-    for n in nums:
-        sum += n
-        res.append(sum)
-    return res
+    total = 0
+    return [total := n + total for n in nums]
 
 
 print(runningSum([1, 2, 3, 4]))
